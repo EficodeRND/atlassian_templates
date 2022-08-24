de3c095b41b9d220ccfed5ecdae0c3c7c079c5ad
Aapo Romu <aapo.romu@eficode.com>
2022-08-24 13:12:13 +0300

    Default for the project lead

--- create_jira_project_from_template.py
+++ create_jira_project_from_template.py
@@ -22,12 +22,15 @@ if __name__ == '__main__':
     jira_url = get_parameter('JIRA_URL', True)
     project_key = get_parameter('PROJECT_KEY', True)
     project_name = get_parameter('PROJECT_NAME', True)
-    project_lead = get_parameter('PROJECT_LEAD', True)
+    project_lead = get_parameter('PROJECT_LEAD', False)
     jira_project_type = get_parameter('JIRA_PROJECT_TYPE', True)
     template_project_id = get_parameter('TEMPLATE_PROJECT_ID', True)
     template_board_ids = get_parameter('TEMPLATE_BOARD_IDS', False)
     jira_username = get_parameter('JIRA_USERNAME', True)
     jira_token = get_parameter('JIRA_TOKEN', True)
+
+    if project_lead is None:
+        project_lead = jira_username
 
     project_url, project_id = jira_service.create_project_from_template(jira_url=jira_url,
                                                                         project_key=project_key,
