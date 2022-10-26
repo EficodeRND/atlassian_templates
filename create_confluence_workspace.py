aada58c4da62d89c5f0df4322a95de995c8e3256
Joni Honkaniemi <joni.honkaniemi@eficode.com>
2022-10-26 10:04:00 +0300

    Beginning the confluence space creation automation

---
+++ create_confluence_workspace.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from asyncio import protocols
+import os
+import json
+import logging as log
+import sys
+
+from services import confluence_service
+from utils.validators import get_parameter
+
+root = log.getLogger()
+root.setLevel(log.DEBUG)
+log_handler = log.StreamHandler(sys.stdout)
+log_handler.setLevel(log.DEBUG)
+formatter = log.Formatter('%(asctime)s %(levelname)s (%(filename)s:%(lineno)d) - %(message)s')
+log_handler.setFormatter(formatter)
+root.addHandler(log_handler)
+
+if __name__ == '__main__':
+    confluence_url = get_parameter('CONFLUENCE_URL', True)              # Confluence URL address
+    project_key = get_parameter('PROJECT_KEY', True)                    # Project/space key
+    project_name = get_parameter('PROJECT_NAME', True)                  # Project/space name
+    confluence_username = get_parameter('CONFLUENCE_USERNAME', True)    # Confluence username for user that will create a space
+    confluence_token = get_parameter('CONFLUENCE_TOKEN', True)          # Confluence personal access token (PAT) that will be used to authenticate
+    category = get_parameter('CATEGORY', True)                          # Category for the space
+
+    workspace_url = confluence_service.create_empty_workspace(confluence_url=confluence_url, project_key=project_key,
+                                                              project_name=project_name,
+                                                              confluence_username=confluence_username,
+                                                              confluence_token=confluence_token,
+                                                              category=category)
---
+++ services/confluence_service.py
@@ -0,0 +1,34 @@
+import logging as log
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+HEADERS = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+
+def create_empty_workspace(confluence_url: str, project_key: str, project_name: str, confluence_username: str, confluence_token: str, category: str):
+    log.info("Creating empty Confluence space")
+
+    authentication = HTTPBasicAuth(confluence_username, confluence_token)
+
+    payload = {
+        "key": project_key,
+        "name": project_name,
+        "description": {
+            "plain": {
+                "value": "This is an example space",
+                "representation": "plain"
+            }
+        },
+        "metadata": {}
+    }
+
+    url = f"{confluence_url}/rest/api/space"
+    response = requests.post(url, json=payload, headers=HEADERS, auth=authentication)
+    if response.status_code != 200:
+        raise Exception(f"Space creation failed: {response.text}")
+
+    return f"{confluence_url}/rest/space"
