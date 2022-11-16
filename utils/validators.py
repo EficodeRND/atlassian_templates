1b277c6198520d64e8130d351850d4c10d2418fc
Aapo Romu <aapo.romu@eficode.com>
2022-11-16 13:30:53 +0200

    Update validators.py
    Fix get parameter utility

--- utils/validators.py
+++ utils/validators.py
@@ -5,12 +5,12 @@ class ValidationException(Exception):
     pass
 
 
-def get_parameter(name: str, mandatory: bool):
+def get_parameter(name: str, mandatory: bool = True):
     value = os.environ.get(name, None)
-    if mandatory:
-        if value is None:
+    if mandatory and value is None:
             raise ValidationException(f"Environment variable {name} not defined")
-
-    if (value.startswith('http://') or value.startswith('https://')) and value.endswith('/'):
-        value = value.rstrip('/')
+    
+    if value is not None:
+        if (value.startswith('http://') or value.startswith('https://')) and value.endswith('/'):
+            value = value.rstrip('/')
     return value
